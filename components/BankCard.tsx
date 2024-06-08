d541704246686be809007fb4161cfaacc9951ce6
Fatima jaffery <fatimajaffery@Fatimas-MacBook-Pro.local>
2024-06-08 14:08:51 -0500

    update BankCard to include text

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -9,14 +9,27 @@ const BankCard = ({account, userName, showBalance=ture}:CreditCardProps) => {
             href={'/'}
             className="bank-card"
         > 
-        <div className="bank-card_content">
-            <h1 className="text-16 font-semibold text-white">
-                {account.name || userName}
-            </h1>
-            <p className="font-ibm-plex-serif font-black text-white">
-                {formatAmount(account.currentBalance)}
-            </p>
-        </div>
+            <div className="bank-card_content">
+                <h1 className="text-16 font-semibold text-white">
+                    {account.name || userName}
+                </h1>
+                <p className="font-ibm-plex-serif font-black text-white">
+                    {formatAmount(account.currentBalance)}
+                </p>
+            </div>
+            <article className="flex flex-col gap-2">
+                <div className="flex justify-between">
+                    <h1 className="text-12 font-semibold text-white">
+                        {userName}
+                    </h1>
+                    <h2 className="text-12 font-semibold text-white">
+                        **/**
+                    </h2>
+                </div>
+                <p className="text-14 font-semibold tracking-[1.1px] text-white">
+                    **** **** **** <span className="text-16">{account.mask}</span>
+                </p>
+            </article>
         </Link>
     </div>
   )
